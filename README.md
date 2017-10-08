4d699758da9ed14edff306b71466ddbb3a9351b8
githop <tom@githop.com>
2017-10-08 15:14:44 -0600

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ossb-foundation
---
+++ app.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', run);
+
+function run() {
+  var splashContent = document.getElementById('ossb-spash-content');
+  var contentDiv = document.getElementById('ossb-content');
+  var scrollContainer = document.getElementById('scroll-container');
+  var splashOffsetTop = splashContent.offsetTop;
+  var threshold = 150;
+
+  splashContent.style.opacity = '0.98';
+  splashContent.style.transition = '0.33s';
+
+  scrollContainer.addEventListener('scroll', (ev) => {
+    var currOffset = contentDiv.getBoundingClientRect().top;
+
+    if (currOffset <= splashOffsetTop + threshold) {
+      splashContent.style.opacity = '0';
+    } else {
+      splashContent.style.opacity = '0.98';
+    }
+
+  })
+}
